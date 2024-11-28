6a2aa7c023228c0a85293677660ca7a80ecbf6d8
OhadZvi <ohad.zvi@post.runi.ac.il>
2024-11-28 12:27:51 +0200

    task completed

--- Primes.java
+++ Primes.java
@@ -1,5 +1,22 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        int primeCounter = 0;
+        boolean[] isNotPrime = new boolean[n + 1];
+        int sqrt_n = (int) Math.ceil(Math.sqrt(n));
+        for (int p = 2; p <= sqrt_n; p++) {
+            for (int i = p * 2; i < isNotPrime.length; i += p) {
+                isNotPrime[i] = true;
+            }
+        }
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 0; i < isNotPrime.length; i++){
+            if (!isNotPrime[i]) {
+                System.out.println(i);
+                primeCounter++;
+            }
+        }
+        int primePercentage = (int) ((n / primeCounter) * 100);
+        System.out.println("There are " + primeCounter + " primes between 2 and " + n + "(" + primePercentage + "% are primes)");
     }
 }
